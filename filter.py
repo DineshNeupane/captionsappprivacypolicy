f3620936199428206b6e05c48ec1cdd376de6abd
DineshNeupane <dinesh.neupane5@gmail.com>
2018-01-28 15:13:34 +0545

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,11 @@
+__author__ = 'Dinesh'
+
+
+for day in range(1, 4):
+    read_file_path = 'C:\\Users\\Dinesh\\Desktop\\Files\\bmcl0'+ str(day).zfill(2)+'-2008-01-' + str(day).zfill(2)+'.Std';
+    new_file_path='C:\\Users\\Dinesh\\Desktop\\NewFiles\\bmcl001'+ str(day).zfill(2)+'-2008-01-' + str(day).zfill(2)+'.txt';
+    new_file=open(new_file_path, 'w')
+    with open(read_file_path, 'r') as f:
+        lines = f.readlines()
+        for x in range(0, 1440, 60):
+            new_file.write(lines[x])
